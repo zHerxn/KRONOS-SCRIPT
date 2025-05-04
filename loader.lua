f527a4b115d5e10ae29d5c49e057da6db10fc131
zHerxn <110173869+zHerxn@users.noreply.github.com>
2025-05-04 16:58:11 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,17 @@
+--[[
+$$\   $$\ $$$$$$$\   $$$$$$\  $$\   $$\  $$$$$$\   $$$$$$\  
+$$ | $$  |$$  __$$\ $$  __$$\ $$$\  $$ |$$  __$$\ $$  __$$\ 
+$$ |$$  / $$ |  $$ |$$ /  $$ |$$$$\ $$ |$$ /  \__|$$ /  \__|
+$$$$$  /  $$$$$$$  |$$ |  $$ |$$ $$\$$ |\$$$$$$\  \$$$$$$\  
+$$  $$<   $$  __$$< $$ |  $$ |$$ \$$$$ | \____$$\  \____$$\ 
+$$ |\$$\  $$ |  $$ |$$ |  $$ |$$ |\$$$ |$$\   $$ |$$\   $$ |
+$$ | \$$\ $$ |  $$ | $$$$$$  |$$ | \$$ |\$$$$$$  |\$$$$$$  |
+\__|  \__|\__|  \__| \______/ \__|  \__| \______/  \______/ 
+                                                            
+]]--
+
+-- Para ejecutar Kronos Script, simplemente copiá esto en tu executor:
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/zHerxn/Kronos-Script/refs/heads/main/Kronos.lua"))()
+
+-- O directamente ejecutá esto:
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zHerxn/Kronos-Script/refs/heads/main/Kronos.lua"))()
